08b3227d59cb7f3fc522f5d0e8208c0c9818ead2
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-02-28 21:28:27 -0600

    settings file and class created and imported to AV

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,16 @@ import sys #contains tools to exit the game when the player quits
 
 import pygame #contains the functionality we need to make the game
 
+from settings import Settings #importing Settings
+
 class AlienInvasion: #Overall class to manage game assets and behavior
     def __init__(self): #Initialize the game, and create game resources
         pygame.init() #initializes the background settings
-        self.screen = pygame.display.set_mode((1200, 800)) #to create a display window for all the game's graphical elements, called a surface, available in all methods in the class
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) #to create a display window for all the game's graphical elements, called a surface, available in all methods in the class
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (230,230,230) #to set background color
+        #self.bg_color = (230,230,230) #to set background color
 
     def run_game(self): #Start the main loop for the game
         while True: #manages screen updates, runs continually
@@ -16,7 +19,7 @@ class AlienInvasion: #Overall class to manage game assets and behavior
                 if event.type == pygame.QUIT: 
                     sys.exit()
 
-                self.screen.fill(self.bg_color) #to fill the background and redraw the screen during each pass thru the loop
+                self.screen.fill(self.settings.bg_color) #to fill the background and redraw the screen during each pass thru the loop
     
                 pygame.display.flip() # Make the most recently drawn screen visible
 if __name__ == '__main__':
