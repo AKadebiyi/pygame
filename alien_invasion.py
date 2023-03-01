6a86a4e87499dac141e37cde0439551975d5fd2c
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-02-28 21:07:44 -0600

    class and game instance created. runs smoothly

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,20 @@
-import sys
+import sys #contains tools to exit the game when the player quits
 
-import pygame
+import pygame #contains the functionality we need to make the game
 
-class AlienInvasion:
-   """Overall class to manage game assets and behavior."""
-   def __init__(self):
-      """Initialize the game, and create game resources."""
-      pygame.init()
-      self.screen = pygame.display.set_mode((1200, 800))
-      pygame.display.set_caption("Alien Invasion")
+class AlienInvasion: #Overall class to manage game assets and behavior
+    def __init__(self): #Initialize the game, and create game resources
+        pygame.init() #initializes the background settings
+        self.screen = pygame.display.set_mode((1200, 800)) #to create a display window for all the game's graphical elements, called a surface, available in all methods in the class
+        pygame.display.set_caption("Alien Invasion")
 
-   def run_game(self):
-    """Start the main loop for the game."""
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                sys.exit()
-# Make the most recently drawn screen visible.
-            pygame.display.flip()
-            if __name__ == '__main__':
-# Make a game instance, and run the
-                game. ai = AlienInvasion()
-                ai.run_game()#Watch for keyboard and mouse events.
+    def run_game(self): #Start the main loop for the game
+        while True: #manages screen updates, runs continually
+            for event in pygame.event.get(): #accessor method that pygame uses to access events it detects
+                if event.type == pygame.QUIT: 
+                    sys.exit()
+    
+                pygame.display.flip() # Make the most recently drawn screen visible
+if __name__ == '__main__':
+    ai = AlienInvasion() # Makes a game instance, 
+    ai.run_game() #and runs the game
