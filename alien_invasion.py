12001ae21003db45476ca9affbe2264768b46704
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-02-28 21:16:56 -0600

    set background color and fill background

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,11 +8,15 @@ class AlienInvasion: #Overall class to manage game assets and behavior
         self.screen = pygame.display.set_mode((1200, 800)) #to create a display window for all the game's graphical elements, called a surface, available in all methods in the class
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (230,230,230) #to set background color
+
     def run_game(self): #Start the main loop for the game
         while True: #manages screen updates, runs continually
             for event in pygame.event.get(): #accessor method that pygame uses to access events it detects
                 if event.type == pygame.QUIT: 
                     sys.exit()
+
+                self.screen.fill(self.bg_color) #to fill the background and redraw the screen during each pass thru the loop
     
                 pygame.display.flip() # Make the most recently drawn screen visible
 if __name__ == '__main__':
