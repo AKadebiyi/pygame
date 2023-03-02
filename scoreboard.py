e973f0a0143053120988e5278c087c11cd1910b3
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-03-01 22:45:44 -0600

    displaying the score

---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+import pygame.font
+
+class Scoreboard: # a class to report scoring info
+    def __init__(self, ai_game): #initalizes the scorekeeping attributes
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        #font settings for scoring info
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        #prepare the initial score image
+        self.prep_score()
+
+    def prep_score(self): #to turn the score in to a rendered image
+        score_str = str(self.stats.score)
+        self.score.image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        #to display the score at the top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self): #to draw score to the screen
+        self.screen.blit(self.score_image, self.score_rect)
