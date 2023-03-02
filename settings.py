fa515efd84e2de2f55a5bd23c1890b81eec88b95
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-03-01 23:05:41 -0600

    testing all hits are scored with increased bullet width

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings: #class to store all settings for Alien Invasion
         self.bg_color = (230,230,230)
 
         #Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3 #limits the number of bullets to 3 at a time
